a65691e02c2baeed28dd525834a3bcb300929b8c
oaxiom <oaxiom@gmail.com>
2021-12-25 08:04:08 +0800

    fixed genome seq download

--- envelope_evolution/2.Genomes/0.get_genomes.sh
+++ envelope_evolution/2.Genomes/0.get_genomes.sh
@@ -1,8 +1,12 @@
 # Human +ve control genome
+wget -c "http://ftp.ensembl.org/pub/release-105/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz"
 
 # Hagfish Eptatretus burgeri 
+# No primary assembly, so use all the scaffold:
+wget -c "http://ftp.ensembl.org/pub/release-105/fasta/eptatretus_burgeri/dna/Eptatretus_burgeri.Eburgeri_3.2.dna.toplevel.fa.gz"
 
 # Lamprey 
+wget -c "http://ftp.ensembl.org/pub/release-105/fasta/petromyzon_marinus/dna/Petromyzon_marinus.Pmarinus_7.0.dna.toplevel.fa.gz"
 
 # Amphioxis
-
+wget -c "http://ftp.ensemblgenomes.org/pub/metazoa/release-52/fasta/branchiostoma_lanceolatum/dna/Branchiostoma_lanceolatum.BraLan2.dna.toplevel.fa.gz"
